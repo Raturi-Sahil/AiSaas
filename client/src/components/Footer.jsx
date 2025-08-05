877cd685851ecf4d8b3d6a0a6758453779b4abd8
Sahil Raturi <sahilraturi687@gmail.com>
2025-08-05 15:46:32 +0530

    fix footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,15 +15,15 @@ function Footer() {
     }
   return (
    <div>
-         <div className='grid sm:grid-cols-4 lg:grid-cols-9 gap-4 mx-10 py-5 border-b border-gray-300'>
-        <div className='sm:col-span-3 sm:mr-8 max-w-md'>
+         <div className='grid sm:grid-cols-5 md:grid-cols-9 gap-4 mx-10 md:mx-15 py-5 border-b border-gray-300'>
+        <div className='sm:col-span-4 md:col-span-3 lg:col-span-4 sm:mr-8 max-w-md'>
             <img src={assets.logo} alt="logo" className='cursor-pointer' onClick={handleClick}/>
             <p className='text-gray-500 text-sm my-5'>
                 Experience the power of AI with OneSolutionAi. <br />
                 Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your workflow.
             </p>
         </div>
-        <div className='sm:col-start-4 sm:col-span-1 lg:col-span-2'>
+        <div className='sm:col-span-1 md:col-span-2'>
            <div>
              <h1 className='font-medium'>Company</h1>
             <ul className='my-5'>
@@ -34,12 +34,12 @@ function Footer() {
             </ul>
            </div>
         </div>
-        <div className='sm:col-span-6 lg:col-span-4 sm:text-center'>
-            <h1 className='font-medium lg:text-justify'>Subscribe to our newletter</h1>
-            <p className='text-gray-500 text-sm my-5 lg:text-justify'>
+        <div className='sm:col-span-6 md:col-span-4 lg:col-span-3 sm:text-center'>
+            <h1 className='font-medium md:text-justify'>Subscribe to our newletter</h1>
+            <p className='text-gray-500 text-sm my-5 md:text-justify'>
                 The latest news, articles, and resources, sent to your inbox weekly.
             </p>
-            <div className='flex lg:justify-start sm:justify-center'>
+            <div className='flex md:justify-start sm:justify-center'>
                 <input type="email" className='text-gray-500 rounded-md border border-gray-300 p-1 mr-1 w-xs text-md' placeholder='Enter your email' />
                 <button className='text-white rounded-md border border-gray-300 py-1 px-2 bg-blue-500 cursor-pointer text-sm' >Subscribe</button>
             </div>
