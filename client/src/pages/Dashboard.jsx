635a945db7a802d16e459c51236784edb32eda81
Sahil Raturi <sahilraturi687@gmail.com>
2025-08-08 02:22:59 +0530

    Dashboard page and footer fine tune

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { dummyCreationData } from '../assets/assets'
+import { Sparkles, Gem } from 'lucide-react';
+import { Protect } from '@clerk/clerk-react';
+import { CreationItem } from '../components/index'
 function Dashboard() {
+
+  const [creations, setCreations] = useState([]);
+  const getDashboardData = async () => {
+    setCreations( dummyCreationData );
+  }
+
+  useEffect(()=> {
+    getDashboardData();
+  }, []);
+
   return (
-    <div>Dashboard</div>
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex justify-start gap-4 flex-wrap'>
+        {/**Total creation card */}
+        <div className='bg-white flex justify-between items-center w-70 p-6 px-10 rounded-xl border border-gray-200'>
+            <div className='text-slate-600'>
+              <h1 className='text-sm'>Total Creations</h1>
+              <p className='text-xl font-semibold'>{creations.length}</p>
+            </div>
+            <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
+              <Sparkles className='w-4'/>
+            </div>
+        </div>
+        {/**Active plan card */}
+        <div className='bg-white flex justify-between items-center w-70 p-6 px-10 rounded-xl border border-gray-200'>
+            <div className='text-slate-600'>
+              <h1 className='text-sm'>Active Plan</h1>
+              <p className='text-xl font-semibold'>
+                <Protect plan='premium' fallback='free'>Premium</Protect> 
+              </p>
+            </div>
+            <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center'>
+              <Gem className='w-4'/>
+            </div>
+        </div>
+      </div>
+
+      <div className='space-y-4'>
+          <h1 className='mt-6'>Recent Creations</h1>
+          {creations.map((item) => (
+            <CreationItem key={item.id} item={item}/>
+          ))}
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -11,12 +11,12 @@ function Layout() {
   const navigate  = useNavigate();
 
   return user ? (
-    <div className='flex flex-col items-start justify-start min-h-screen'>
-      <nav className='px-8 w-full min-h-14 flex items-center justify-between border-b border-gray-200'>
+    <div className='flex flex-col items-start justify-start h-screen'>
+      <nav className='px-8 w-full h-16 flex items-center justify-between border-b border-gray-200'>
         <img src={assets.logo} alt="logo" className='w-32 sm:w-40 cursor-pointer' onClick={() => navigate('/')}/>
         { sidebar? <X onClick={()=> setSidebar(false)} className='w-6 h-6 text-gray-600 sm:hidden'/>: <Menu onClick={()=> setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden'/>}
       </nav>
-      <div className='flex-1 w-full flex'>
+      <div className='flex-1 w-full flex h-[calc(100vh-64px)]'>
             <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
             <div className='flex-1 bg-[#F4F7FB]'>
                <Outlet />
@@ -28,6 +28,6 @@ function Layout() {
           <SignIn />
       </div>
   )
-}
+}   
 
 export default Layout
